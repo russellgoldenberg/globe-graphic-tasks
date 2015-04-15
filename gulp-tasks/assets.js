fa8e85f9732725520fd5aff5471093f7f5917e4c
russell goldenberg <russellgoldenberg@gmail.com>
2015-04-15 12:57:35 -0400

    removed dev reference

--- gulp-tasks/assets.js
+++ gulp-tasks/assets.js
@@ -3,13 +3,6 @@ var changed = require('gulp-changed');
 
 var src = 'src/assets/**/*';
 
-//move assets files to dev folder
-gulp.task('assets-dev', function(cb) {
-	return gulp.src(src)
-		.pipe(changed('dev/assets'))
-		.pipe(gulp.dest('dev/assets'));
-});
-
 //move assets files to prod folder
 gulp.task('assets-prod', function(cb) {
 	return gulp.src(src)
--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 // Default task to be run with `gulp`
-gulp.task('default', ['clean-dev', 'css-dev', 'copy-dev', 'assets-dev', 'browser-sync'], function () {
+gulp.task('default', ['clean-dev', 'css-dev', 'copy-dev', 'browser-sync'], function () {
     gulp.watch('src/css/*.styl', ['css-dev']);
     gulp.watch('src/data/copy.json', ['copy-dev']);
     gulp.watch('src/index.hbs', ['copy-dev']);
