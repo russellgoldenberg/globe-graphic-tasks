a4a50d5b6bd724921fa446168f29656e38b07ecb
russell goldenberg <russellgoldenberg@gmail.com>
2015-05-28 09:27:33 -0400

    watch all json

--- gulp-tasks/default.js
+++ gulp-tasks/default.js
@@ -4,7 +4,7 @@ var runSequence = require('run-sequence');
 // Default task to be run with `gulp`
 gulp.task('default', ['dev'], function () {
     gulp.watch('src/css/*.styl', ['css-dev']);
-    gulp.watch('src/data/copy.json', ['html-dev']);
+    gulp.watch('src/data/*.json', ['html-dev']);
     gulp.watch('src/html/**/*.hbs', ['html-dev']);
     gulp.watch('src/index.html', ['browser-sync-reload']);
     gulp.watch('src/assets/**/*', ['browser-sync-reload']);
