3016e35d204850ee50b5d6b428749076a862cb96
russell goldenberg <russellgoldenberg@gmail.com>
2015-06-03 15:12:16 -0400

    remove cruft

--- gulp-tasks/assets.js
+++ gulp-tasks/assets.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var changed = require('gulp-changed');
-// var imagemin = require('gulp-imagemin');
 
 var src = 'src/assets/**/*';
 
@@ -10,14 +9,4 @@ gulp.task('assets-prod', function() {
 		.pipe(changed('prod/assets'))
 		.pipe(gulp.dest('prod/assets'));
 });
-
-//minify images
-// gulp.task('assets-prod', function () {
-//     return gulp.src(src)
-//         .pipe(imagemin({
-//             progressive: true,
-//             interlaced: true
-//         }))
-//         .pipe(gulp.dest('prod/assets'));
-// });
 
