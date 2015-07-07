c445aee843375c7d9455b35bb1f57c170855c955
russell goldenberg <russellgoldenberg@gmail.com>
2015-07-07 12:24:09 -0400

    add new stylus features

--- gulp-tasks/css.js
+++ gulp-tasks/css.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
-var cmq = require('gulp-combine-media-queries');
 var minifycss = require('gulp-minify-css');
 var browserSync = require('browser-sync');
 var plumber = require('gulp-plumber');
+var replace = require('gulp-replace');
 
 //compile styl to css and autoprefix
 gulp.task('css-dev', function () {
-	gulp.src('src/css/dev.styl')
+	gulp.src('src/css/config.styl')
 		.pipe(plumber({
 	        errorHandler: function (err) { console.log(err); this.emit('end'); }
 	    }))
@@ -20,12 +20,12 @@ gulp.task('css-dev', function () {
 		.pipe(browserSync.reload({stream:true}));
 });
 
-//compile all styl and autoprefix, combine media queries, and minify
+//compile all styl and autoprefix, and minify
 gulp.task('css-prod', function() {
-	gulp.src('src/css/prod.styl')
+	gulp.src('src/css/config.styl')
         .pipe(stylus())
 		.pipe(autoprefixer())
-		.pipe(cmq({ log: true }))
+		.pipe(replace(/\.\.\/assets/g, 'assets'))
 		.pipe(minifycss())
 		.pipe(rename('main.css'))
 		.pipe(gulp.dest('.tmp/css'))	
